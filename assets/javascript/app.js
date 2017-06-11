4708643531b01bf77813e4cf5d95bebb23f05fff
Jake Thornberry <wjthornberry@gmail.com>
2017-06-11 16:10:08 -0500

    Add AJAX call and new GIFs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,12 +33,34 @@ $('addCharacter').on('click', function(){
     // If users hit enter instead of clicking the button
     return false;
 })
-// dynamically add five buttons to HTML
 
-// each button has the name of a different Arrested Development character
+// click on button, ten GIFs related to that button are displayed via AJAX call
+function displayGIFs(){
+    var character = $(this).attr('data-name');
 
-// click on button, nine GIFs related to that button are displayed via AJAX call
+    // Calls AJAX
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response){
+        console.log(response.data);
 
+        var results = response.data;
+
+        for (var i = 0; results.length; i++) {
+            var gifDiv = $('<div class=GIFs>');
+            var characterGIF = $('<img>');
+                characterGIF.attr('src', results[i].images.fixed_heigh_still.url);
+                characterGIF.attr('title', "Rating: " + results[i].rating);
+                characterGIF.attr('data-still', results[i].images.fixed_height_still.url);
+                characterGIF.attr('data-state', 'still');
+                characterGIF.addClass('gif');
+                characterGIF.attr('data-animate', results[i].images.fixed_heigh.url);
+            gifDiv.append(characterGIF);
+            $('.addCharacters');
+        };
+    });
+};
 // each GIF displays its rating (e.g., "G," "PG," etc.) above it
 
 // click on GIF, it animates
