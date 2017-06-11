50e071cb7c0e4d81658a6d6c7fda72206efd116b
Jake Thornberry <wjthornberry@gmail.com>
2017-06-10 21:30:29 -0500

    pseudocode pass 1

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// dynamically add five buttons to HTML
+
+// each button has the name of a different Arrested Development character
+
+// click on button, nine GIFs related to that button are displayed
+
+// each GIF displays its rating (e.g., "G," "PG," etc.)
+
+// click on GIF, it animates
+
+// click again, it stops
