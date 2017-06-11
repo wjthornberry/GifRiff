56cd5b9181d5b8a4b44d29c255cf0de2321a0ec2
Jake Thornberry <wjthornberry@gmail.com>
2017-06-10 22:05:55 -0500

    Pseudocode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,8 +4,10 @@
 
 // click on button, nine GIFs related to that button are displayed
 
-// each GIF displays its rating (e.g., "G," "PG," etc.)
+// each GIF displays its rating (e.g., "G," "PG," etc.) above it
 
 // click on GIF, it animates
 
 // click again, it stops
+
+// Type another Arrested Development word or character into text box, adds button to list above
